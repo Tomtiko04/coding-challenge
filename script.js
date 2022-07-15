f5ae51c38ef7185a5596aa13bc6028c7bf7252fa
Tomtiko04 <83877877+Tomtiko04@users.noreply.github.com>
2022-07-15 22:55:07 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var markBMI = Math.round(78 / (1.69 ** 2));
+var johnBMI = Math.round(95 / (1.95 ** 2));
+console.log(markBMI, johnBMI);
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI is higher than John's!`);
+    //Coding Challenge #2
+    console.log(`Mark's BMI(${markBMI}) is higher than John's (${johnBMI})`);
+} else {
+    console.log(`John's BMI is higher than Mark's!`);
+    //Coding Challenge #2
+    console.log(`John's BMI(${johnBMI}) is higher than Mark's (${markBMI})`);
+}
